b9780f603241b718769133221082b2c66d292186
Shaun Payne <convale@gmail.com>
2020-09-21 15:51:35 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,107 @@
-# TOP-TicTacToe
+<!-- PROJECT SHIELDS -->
 
-Who doesn't love a good game of Tic-Tac-Toe!
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
 
-Player win count and visual representation with green background for the winner's three in a row.
+<!-- PROJECT LOGO -->
+<p align="center">
 
-Have fun!
+  <h3 align="center">TOP-TicTacToe</h3>
+
+  <p align="center">
+   A fun basic game of TicTacToe you can play with a friend!
+    <br />
+    <br />
+    <a href="hhttps://convale.github.io/TOP-TicTacToe/">View Demo</a>
+    ·
+    <a href="https://github.com/Convale/TOP-TicTacToe/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/Convale/TOP-TicTacToe/issues">Request Feature</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [About the Project](#about-the-project)
+  - [Built With](#built-with)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+- [Roadmap](#roadmap)
+- [Contributing](#contributing)
+- [License](#license)
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+[![TicTacToe Screen Shot][product-screenshot]](https://convale.github.io/TOP-TicTacToe/)
+
+Who doesn't love a good game of Tic-Tac-Toe! This is part of The ODIN Project's Fullstack Javascript Bootcamp to teach budding web developers the basics. This is one of the beginning lessons and built with basic technologies.
+
+### Built With
+
+This game was built using pure HTML, CSS, and JS.
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+Since the project is built with pure HTML, CSS, and JS, no dependencies are required to install.
+
+### Installation
+
+1. Clone the repo
+
+```sh
+git clone https://github.com/Convale/TOP-TicTacToe.git
+```
+
+<!-- ROADMAP -->
+
+## Roadmap
+
+See the [open issues](https://github.com/Convale/TOP-TicTacToe/issues) for a list of proposed features (and known issues).
+
+Future revisions include:
+
+1. Revamping the code for checking if a game has been won or tied
+
+<!-- CONTRIBUTING -->
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+
+## License
+
+Distributed under the MIT License. See [`LICENSE`](https://github.com/Convale/TOP-TicTacToe/blob/master/LICENSE) for more information.
+
+<!-- MARKDOWN LINKS & IMAGES -->
+
+[contributors-shield]: https://img.shields.io/github/contributors/Convale/TOP-TicTacToe
+[contributors-url]: https://github.com/Convale/TOP-TicTacToe/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/Convale/TOP-TicTacToe
+[forks-url]: https://github.com/Convale/TOP-TicTacToe/network/members
+[stars-shield]: https://img.shields.io/github/stars/Convale/TOP-TicTacToe
+[stars-url]: https://github.com/Convale/TOP-TicTacToe/stargazers
+[issues-shield]: https://img.shields.io/github/issues/Convale/TOP-TicTacToe
+[issues-url]: https://github.com/Convale/TOP-TicTacToe/issues
+[license-shield]: https://img.shields.io/github/license/Convale/TOP-TicTacToe
+[license-url]: https://github.com/Convale/TOP-TicTacToe/blob/master/LICENSE
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/payneshaun
+[product-screenshot]: tictactoe.png
--- scripts.js
+++ scripts.js
@@ -1,131 +1,165 @@
 let gameValues = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = 1;
 
-const Player = function() {
-    this.wins = 0;
-    this.incrementWin = function() {
-        this.wins++
-    };
-}
+const Player = function () {
+  this.wins = 0;
+  this.incrementWin = function () {
+    this.wins++;
+  };
+};
 const player1 = new Player();
 const player2 = new Player();
 
 function takeTurn(details) {
-    const tileNum = parseInt(details.target.getAttribute("id"));
-    if (gameValues[tileNum] == "" && currentPlayer != 0) {
-        const currentPlayerIcon = (currentPlayer == 1) ? "X" : "O";
-        gameValues[tileNum] = currentPlayerIcon;
-        gameBoard();
-        checkWin();
-        currentPlayer = (currentPlayer == 1) ? 2 : (currentPlayer == 2) ? 1 : 0;
-    }
-}
-
-//this function is terribly written: manually set and should ultimately be condensed to an interative function. It's 72 lines!!!!
-function checkWin() {
-    const currentPlayerIcon = (currentPlayer == 1) ? "X" : "O";
-    if (gameValues[0] == currentPlayerIcon && gameValues[1] == currentPlayerIcon && gameValues[2] == currentPlayerIcon) {
-        document.getElementById('0').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('1').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('2').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[3] == currentPlayerIcon && gameValues[4] == currentPlayerIcon && gameValues[5] == currentPlayerIcon)  {
-        document.getElementById('3').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('4').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('5').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[6] == currentPlayerIcon && gameValues[7] == currentPlayerIcon && gameValues[8] == currentPlayerIcon) {
-        document.getElementById('6').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('7').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('8').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[0] == currentPlayerIcon && gameValues[3] == currentPlayerIcon && gameValues[6] == currentPlayerIcon) {
-        document.getElementById('0').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('3').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('6').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[1] == currentPlayerIcon && gameValues[4] == currentPlayerIcon && gameValues[7] == currentPlayerIcon) {
-        document.getElementById('1').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('4').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('7').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[2] == currentPlayerIcon && gameValues[5] == currentPlayerIcon && gameValues[8] == currentPlayerIcon) {
-        document.getElementById('2').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('5').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('8').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[0] == currentPlayerIcon && gameValues[4] == currentPlayerIcon && gameValues[8] == currentPlayerIcon) {
-        document.getElementById('0').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('4').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('8').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[2] == currentPlayerIcon && gameValues[4] == currentPlayerIcon && gameValues[6] == currentPlayerIcon) {
-        document.getElementById('2').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('4').style.backgroundColor = 'rgb(149, 186, 147)';
-        document.getElementById('6').style.backgroundColor = 'rgb(149, 186, 147)';
-        (currentPlayer == 1) ? player1.incrementWin() : player2.incrementWin();
-        playerWinsBanner();
-        currentPlayer = 0;
-    }
-    else if (gameValues[0] != "" && gameValues[1] != "" && gameValues[2] != "" && gameValues[3] != "" && gameValues[4] 
-    != "" && gameValues[5] != "" && gameValues[6] != "" && gameValues[7] != "" && gameValues[8] != "") {
-        alert("tie");
-        currentPlayer = 0;
-    }
-}
-
-startButton.addEventListener("click", ()=> {    
-    gameValues = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = 1;
+  const tileNum = parseInt(details.target.getAttribute("id"));
+  if (gameValues[tileNum] == "" && currentPlayer != 0) {
+    const currentPlayerIcon = currentPlayer == 1 ? "X" : "O";
+    gameValues[tileNum] = currentPlayerIcon;
     gameBoard();
-})
+    checkWin();
+    currentPlayer = currentPlayer == 1 ? 2 : currentPlayer == 2 ? 1 : 0;
+  }
+}
+
+function checkWin() {
+  const currentPlayerIcon = currentPlayer == 1 ? "X" : "O";
+  if (
+    gameValues[0] == currentPlayerIcon &&
+    gameValues[1] == currentPlayerIcon &&
+    gameValues[2] == currentPlayerIcon
+  ) {
+    document.getElementById("0").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("1").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("2").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[3] == currentPlayerIcon &&
+    gameValues[4] == currentPlayerIcon &&
+    gameValues[5] == currentPlayerIcon
+  ) {
+    document.getElementById("3").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("4").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("5").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[6] == currentPlayerIcon &&
+    gameValues[7] == currentPlayerIcon &&
+    gameValues[8] == currentPlayerIcon
+  ) {
+    document.getElementById("6").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("7").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("8").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[0] == currentPlayerIcon &&
+    gameValues[3] == currentPlayerIcon &&
+    gameValues[6] == currentPlayerIcon
+  ) {
+    document.getElementById("0").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("3").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("6").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[1] == currentPlayerIcon &&
+    gameValues[4] == currentPlayerIcon &&
+    gameValues[7] == currentPlayerIcon
+  ) {
+    document.getElementById("1").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("4").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("7").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[2] == currentPlayerIcon &&
+    gameValues[5] == currentPlayerIcon &&
+    gameValues[8] == currentPlayerIcon
+  ) {
+    document.getElementById("2").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("5").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("8").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[0] == currentPlayerIcon &&
+    gameValues[4] == currentPlayerIcon &&
+    gameValues[8] == currentPlayerIcon
+  ) {
+    document.getElementById("0").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("4").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("8").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[2] == currentPlayerIcon &&
+    gameValues[4] == currentPlayerIcon &&
+    gameValues[6] == currentPlayerIcon
+  ) {
+    document.getElementById("2").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("4").style.backgroundColor = "rgb(149, 186, 147)";
+    document.getElementById("6").style.backgroundColor = "rgb(149, 186, 147)";
+    currentPlayer == 1 ? player1.incrementWin() : player2.incrementWin();
+    playerWinsBanner();
+    currentPlayer = 0;
+  } else if (
+    gameValues[0] != "" &&
+    gameValues[1] != "" &&
+    gameValues[2] != "" &&
+    gameValues[3] != "" &&
+    gameValues[4] != "" &&
+    gameValues[5] != "" &&
+    gameValues[6] != "" &&
+    gameValues[7] != "" &&
+    gameValues[8] != ""
+  ) {
+    alert("tie");
+    currentPlayer = 0;
+  }
+}
+
+resetButton.addEventListener("click", () => {
+  gameValues = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = 1;
+  gameBoard();
+});
 
 function gameBoard() {
-    const gameArray = document.querySelector('#gameArray');
-    gameArray.innerHTML = '';
+  const gameArray = document.querySelector("#gameArray");
+  gameArray.innerHTML = "";
 
-    for (let i = 0; i < 9; i++) {
-        const gameTile = document.createElement("div");
-        gameTile.setAttribute("class", "boardDiv");
-        gameTile.setAttribute("id", i);
-        gameTile.addEventListener('click', takeTurn);
-        gameArray.appendChild(gameTile);
+  for (let i = 0; i < 9; i++) {
+    const gameTile = document.createElement("div");
+    gameTile.setAttribute("class", "boardDiv");
+    gameTile.setAttribute("id", i);
+    gameTile.addEventListener("click", takeTurn);
+    gameArray.appendChild(gameTile);
 
-        const addElement = document.createElement("il");
-        const textNode = document.createTextNode(gameValues[i]);
-        addElement.setAttribute("id", i);
-        addElement.appendChild(textNode);
-        document.getElementById(i).appendChild(addElement);
-    }
+    const addElement = document.createElement("il");
+    const textNode = document.createTextNode(gameValues[i]);
+    addElement.setAttribute("id", i);
+    addElement.appendChild(textNode);
+    document.getElementById(i).appendChild(addElement);
+  }
 }
 
 function playerWinsBanner() {
-    gameArray = document.querySelector('#player1').innerHTML = "Player 1: " + player1.wins;
-    gameArray = document.querySelector('#player2').innerHTML = "Player 2: " + player2.wins;
+  gameArray = document.querySelector("#player1").innerHTML =
+    "Player 1: " + player1.wins;
+  gameArray = document.querySelector("#player2").innerHTML =
+    "Player 2: " + player2.wins;
 }
 
 (function main() {
-    playerWinsBanner();
-    gameBoard();
-})()
+  playerWinsBanner();
+  gameBoard();
+})();
